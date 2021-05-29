77e4973996b76c52a04b569735657f0b9988406d
Jaxon D <duncan5irjax@gmail.com>
2021-05-29 11:27:20 -0500

    Got the routes to actually switch the page

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,35 @@
 import './App.css';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import { Navbar } from './components/Navbar';
 import SignIn from './pages/SignIn';
+import Notes from './pages/Notes';
+import Reminders from './pages/Reminders';
+import Calendar from './pages/Calendar';
 
 function App() {
   return (
-    <div className="App">
-      <Navbar />
-      <SignIn/>
-    </div>
+    <Router>
+      <div className="App">
+        <Navbar />
+        <Switch>
+          <Route exact path={["/", "/signin"]}>
+            <SignIn />
+          </Route>
+          <Route exact path={["/notes"]}>
+            <Notes />
+          </Route>
+          <Route exact path={["/reminder"]}>
+            <Reminders />
+          </Route>
+          <Route exact path={["/calendar"]}>
+            <Calendar />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
+// Dashboard redirects to login if not logged in
+// Phrasing on data flow
 export default App;
