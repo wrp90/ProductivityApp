9220d0195a654c04f9b63b8250e028d95781a5cb
Dora O <dorozco2390@utexas.edu>
2021-05-29 11:10:29 -0500

    updated dependencies and app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,16 @@
 import './App.css';
 import { Navbar } from './components/Navbar';
 import SignIn from './pages/SignIn';
+import Notes from './pages/Notes';
+import Calendar from 'react-calendar';
 
 function App() {
   return (
     <div className="App">
       <Navbar />
       <SignIn/>
+      <Notes />
+      <Calendar />
     </div>
   );
 }
