f88f326fe77e0560848df25c12b786eecc61ce5a
Will Papp <wpapp90@gmail.com>
2021-06-01 14:23:32 -0500

    added labels to seed data

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,8 +1,9 @@
 const router = require("express").Router();
 const notesRoute = require("./notes");
+// const userRoutes = require("./user")
 
 // User routes
-router.use("/user", userRoutes);
+// router.use("/user", userRoutes);
 // Notes routes
 router.use("/notes", notesRoute);
 
