7f71a624a05a39d8596d88dc8cb806fd34cea016
Will Papp <wpapp90@gmail.com>
2021-06-07 19:49:49 -0500

    added userRoutes

--- routes/api/index.js
+++ routes/api/index.js
@@ -2,10 +2,10 @@ const router = require("express").Router();
 const notesRoute = require("./notes");
 const remindersRoute = require("./reminders");
 const eventsRoute = require("./event");
-// const userRoutes = require("./user")
+const userRoutes = require("./user")
 
 // User routes
-// router.use("/user", userRoutes);
+router.use("/user", userRoutes);
 // Notes routes
 router.use("/notes", notesRoute);
 router.use("/reminders", remindersRoute);
