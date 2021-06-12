7504430c29786ebdf64b1d588077e2e0c756f0d7
Will Papp <wpapp90@gmail.com>
2021-06-12 12:28:06 -0500

    small navbar changes

--- client/src/components/Navbar/index.js
+++ client/src/components/Navbar/index.js
@@ -6,43 +6,45 @@ export function Navbar(props) {
             <div className="d-flex flex-grow-1">
                 <span className="w-100 d-lg-none d-block" />
                 <a className="navbar-brand d-none d-lg-inline-block" href="/"> be productive please </a>
-                <ul className="navbar-nav ms-auto flex-nowrap">
-                    <li className="nav-item">
-                        <a href="/notes" 
-                        className={
-                            window.location.pathname === "/notes"
-                                ? "nav-link active menu-item btn btn-outline-secondary"
-                                : "nav-link menu-item btn btn-outline-secondary"
-                        }>Notes</a>
-                    </li>
-                    <li className="nav-item">
-                        <a href="/reminders" 
-                        className={
-                            window.location.pathname === "/reminders"
-                                ? "nav-link active menu-item btn btn-outline-secondary"
-                                : "nav-link menu-item btn btn-outline-secondary"
-                        }>Reminders</a>
-                    </li>
-                    <li className="nav-item">
-                        <a href="/calendar" 
-                        className={
-                            window.location.pathname === "/calendar"
-                                ? "nav-link active menu-item btn btn-outline-secondary"
-                                : "nav-link menu-item btn btn-outline-secondary"
-                        }>Calendar</a>
-                    </li>
-                    <li className="nav-item">
-                        <a href="/weather" 
-                        className={
-                            window.location.pathname === "/weather"
-                                ? "nav-link active menu-item btn btn-outline-secondary"
-                                : "nav-link menu-item btn btn-outline-secondary"
-                        }>Weather</a>
-                    </li>
-                </ul>
+                <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul className="navbar-nav ms-auto">
+                        <li className="nav-item">
+                            <a href="/notes"
+                                className={
+                                    window.location.pathname === "/notes"
+                                        ? "nav-link active menu-item btn btn-outline-secondary"
+                                        : "nav-link menu-item btn btn-outline-secondary"
+                                }>Notes</a>
+                        </li>
+                        <li className="nav-item">
+                            <a href="/reminders"
+                                className={
+                                    window.location.pathname === "/reminders"
+                                        ? "nav-link active menu-item btn btn-outline-secondary"
+                                        : "nav-link menu-item btn btn-outline-secondary"
+                                }>Reminders</a>
+                        </li>
+                        <li className="nav-item">
+                            <a href="/calendar"
+                                className={
+                                    window.location.pathname === "/calendar"
+                                        ? "nav-link active menu-item btn btn-outline-secondary"
+                                        : "nav-link menu-item btn btn-outline-secondary"
+                                }>Calendar</a>
+                        </li>
+                        <li className="nav-item">
+                            <a href="/weather"
+                                className={
+                                    window.location.pathname === "/weather"
+                                        ? "nav-link active menu-item btn btn-outline-secondary"
+                                        : "nav-link menu-item btn btn-outline-secondary"
+                                }>Weather</a>
+                        </li>
+                    </ul>
+                </div>
                 <div class="w-100 text-right">
-                    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#myNavbar">
-                        <span class="navbar-toggler-icon"></span>
+                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
                     </button>
                 </div>
             </div>
