752cfc9f023ada9efddcd55db0aa8d864a9b0ed7
Dora O <dorozco2390@utexas.edu>
2021-06-15 22:39:48 -0500

    added changes to dashboard so that it was viewable in mobile view

--- client/src/pages/Dashboard.js
+++ client/src/pages/Dashboard.js
@@ -2,10 +2,16 @@ import React from 'react';
 
 function Dashboard() {
     return (
-        <div style={{backgroundColor:'lightblue', height: '100vh'}}>
-            <h1 style={{ fontSize: 75, clear: "both", textAlign: "center"}}>Welcome to your dashboard!</h1>
-            <br></br>
-            <img src="https://i0.wp.com/bestlifeonline.com/wp-content/uploads/2019/12/shutterstock_1290320698.jpg" width="370" height="375" alt ="puppy"></img>
+        <div className="row " style={{ backgroundColor: 'lightblue', height: '100vh' }}>
+            <div className="col-sm-12  ">
+                <div className="row justify-content-center">
+                    <div className="col-sm-4">
+                        <h1 className="w-100" style={{ fontSize: 75 }}>Welcome to your dashboard!</h1>
+                        <br></br>
+                        <img className="w-100" src="https://i0.wp.com/bestlifeonline.com/wp-content/uploads/2019/12/shutterstock_1290320698.jpg" alt="puppy"></img>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
